e2b057de7fa484b55f62c2c650cd7721b84934a3
Nazar Pentsarskyi <nazar.pentsarskyi@gmail.com>
2023-04-01 02:44:46 +0300

    updated datacolection

--- client/src/pages/gallery/gallery.jsx
+++ client/src/pages/gallery/gallery.jsx
@@ -1,73 +1,95 @@
-import React, { useState } from 'react';
-import CloseIcon from '@mui/icons-material/Close';
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import './gallery.css';
-
-import img21 from './img/img21.jpg';
-import img22 from './img/img22.jpg';
-import img23 from './img/img23.jpg';
-import img24 from './img/img24.jpg';
+import React from 'react';
+import { GalleryContent } from './galleryContent';
 
 
 export const Gallery = () => {
 
   let dataCollection = [
     {
-      imgSrc: img21,
+      id: 1,
+      imgSrc: 'https://live.staticflickr.com/65535/52784794268_6e6162c959_o.jpg',
     },
     {
-      imgSrc: img22,
+      id: 2,
+      imgSrc: 'https://live.staticflickr.com/65535/52784577324_6e3d070a5b_o.jpg',
     },
     {
-      imgSrc: img23,
+      id: 3,
+      imgSrc: 'https://live.staticflickr.com/65535/52784577319_b536746ffc_o.jpg',
     },
     {
-      imgSrc: img24,
-    }
+      id: 4,
+      imgSrc: 'https://live.staticflickr.com/65535/52784339366_d6a3bf2d93_o.jpg',
+    },
+    {
+      id: 5,
+      imgSrc: 'https://live.staticflickr.com/65535/52784736130_f5c3197f2e_o.jpg',
+    },
+    {
+      id: 6,
+      imgSrc: 'https://live.staticflickr.com/65535/52783787652_7c6f4d1c7d_o.jpg',
+    },
+    {
+      id: 7,
+      imgSrc: 'https://live.staticflickr.com/65535/52784577259_b04936a5e6_o.jpg',
+    },
+    {
+      id: 8,
+      imgSrc: 'https://live.staticflickr.com/65535/52783787622_480ed089ce_o.jpg',
+    },
+    {
+      id: 9,
+      imgSrc: 'https://live.staticflickr.com/65535/52784577244_c987e9c3f0_o.jpg',
+    },
+    {
+      id: 10,
+      imgSrc: 'https://live.staticflickr.com/65535/52784736085_c657976d13_o.jpg',
+    },
+    {
+      id: 11,
+      imgSrc: 'https://live.staticflickr.com/65535/52784736075_8a080e0dee_o.jpg',
+    },
+    {
+      id: 12,
+      imgSrc: 'https://live.staticflickr.com/65535/52784794153_a0b6968ba2_o.jpg',
+    },
+    {
+      id: 13,
+      imgSrc: 'https://live.staticflickr.com/65535/52784577194_1da0693164_o.jpg',
+    },
+    {
+      id: 14,
+      imgSrc: 'https://live.staticflickr.com/65535/52784577184_5a82c25842_o.jpg',
+    },
+    {
+      id: 15,
+      imgSrc: 'https://live.staticflickr.com/65535/52784339186_dd734fb23c_o.jpg',
+    },
+    {
+      id: 16,
+      imgSrc: 'https://live.staticflickr.com/65535/52784794113_99faa33b94_o.jpg',
+    },
+    {
+      id: 17,
+      imgSrc: 'https://live.staticflickr.com/65535/52783787507_9ce4bc8e9b_o.jpg',
+    },
+    {
+      id: 18,
+      imgSrc: 'https://live.staticflickr.com/65535/52784794093_73e553cee6_o.jpg',
+    },
+    {
+      id: 19,
+      imgSrc: 'https://live.staticflickr.com/65535/52784339146_26ed261114_o.jpg',
+    },
+    {
+      id: 20,
+      imgSrc: 'https://live.staticflickr.com/65535/52783787457_38bcc5a12f_o.jpg',
+    },
   ]
 
-  const [slideNumber, setSlideNumber] = useState(0);
-  const [openModal, setOpenModal] = useState(false);
-
-  const handleOpenModal = (index) => {
-    setSlideNumber(index)
-    setOpenModal(true)
-  }
-  
-  const handleCloseModal = () => {
-    setOpenModal(false)
-  }
-
-  const prevSlide = () => {
-    slideNumber === 0 
-    ? setSlideNumber( dataCollection.length -1 ) 
-    : setSlideNumber( slideNumber - 1 )
-  }
-  
-  const nextSlide = () => {
-    slideNumber + 1 === dataCollection.length 
-    ? setSlideNumber(0) 
-    : setSlideNumber(slideNumber + 1)
-  }
-  
   return (
-  <div className='gallery'>
-    <h2>Фотогалерея</h2>
-    <div className={openModal ? 'model open' : 'model'}>
-      <CloseIcon className='closeIcon' onClick={handleCloseModal} />
-      <ArrowBackIosNewIcon className='prevIcon' onClick={prevSlide} />
-      <img src={dataCollection[slideNumber].imgSrc} alt='' />
-      <ArrowForwardIosIcon className='nextIcon' onClick={nextSlide} />
+    <div>
+      <GalleryContent dataCollection={dataCollection} />
     </div>
-    <div className='gallery__content'>
-      {dataCollection && dataCollection.map((slide, index) => (
-        <div className='gallery__item' key={index} onClick={ () => handleOpenModal(index)}>
-          <img src={slide.imgSrc} alt='' style={{width: '100%', height: '360px'}} />
-        </div>
-        )
-      )}
-    </div>
-  </div>
   )
 }
